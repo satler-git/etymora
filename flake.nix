04b47e850348cc953bf34ea77af098b4398e5430
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-20 21:08:12 +0900

    :sparkles: add a basic flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,92 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "nixpkgs";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = { };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+      ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-  };
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+
+            programs.nixfmt.enable = true;
+            programs.taplo.enable = true;
+            programs.rustfmt.enable = true;
+          };
+
+          pre-commit = {
+            settings.hooks = {
+              flake-treefmt = {
+                enable = true;
+                name = "flake-treefmt";
+                entry = lib.getExe config.treefmt.build.wrapper;
+                pass_filenames = false;
+              };
+
+              clippy.enable = true;
+              cargo-check.enable = true;
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ config.pre-commit.devShell ];
+
+            buildInputs = with pkgs; [
+              cargo-expand
+              cargo-nextest
+              rust-bin.stable.latest.default
+            ];
+          };
+        };
+    };
 }
