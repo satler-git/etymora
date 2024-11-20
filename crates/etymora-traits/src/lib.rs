abc4648df5a272de9597e5eb72db096f3e1730c0
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-20 20:26:15 +0900

    :sparkles: add etymora-traits

--- crates/etymora-traits/src/lib.rs
+++ crates/etymora-traits/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Word(String);
+
+impl Word {
+    pub fn inner(self) -> String {
+        self.0
+    }
+}
+
+impl<T: Into<String>> From<T> for Word {
+    fn from(value: T) -> Self {
+        let w: String = value.into().trim().into();
+        Word(w)
+    }
+}
+
+pub trait Dictionary {
+    type Error;
+
+    fn exits(
+        &self,
+        word: &Word,
+    ) -> impl std::future::Future<Output = Result<bool, Self::Error>> + Send;
+    fn lookup_ditail(
+        &self,
+        word: &Word,
+    ) -> impl std::future::Future<Output = Result<markdown_builder::Markdown, Self::Error>> + Send;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::Word;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_word() {
+        let word: Word = "  lang\n\r".into();
+        assert_eq!(word.inner(), "lang".to_string());
     }
 }
