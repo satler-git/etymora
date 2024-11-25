5dd5ce6bb94b568f84aa7fa693e15f74cd60e44e
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-25 13:14:36 +0900

    :wrench: add example config

---
+++ config-examples/etymora.lua
@@ -0,0 +1,21 @@
+local configs = require("lspconfig.configs")
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
+
+configs.etymora = {
+	default_config = {
+		cmd = { "etymora" },
+		-- filetypes = { "markdown" },
+		root_dir = util.root_pattern(".git", "Cargo.toml"),
+		init_options = {},
+	},
+	docs = {
+		description = [[
+      https://github.com/satler-git/etymora
+
+      Language Server for Dictionaries.
+    ]],
+	},
+}
+
+lspconfig.etymora.setup({})
